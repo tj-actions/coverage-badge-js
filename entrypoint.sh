453c3a079a8bef7af49cb59c5e5b4cdf17d9b5b6
Tonye Jack <jtonye@ymail.com>
2021-04-20 20:50:38 -0400

    Updated script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-cd "${INPUT_PATH}"
-make-coverage-badge --output-path "${INPUT_OUTPUT_PATH}" --report-path "${INPUT_REPORT_PATH}"
+make-coverage-badge --output-path "${INPUT_PATH}/${INPUT_OUTPUT_PATH}" --report-path "${INPUT_PATH}/${INPUT_REPORT_PATH}"
